e48fe0f04c82781ac0818960668e91b050532b03
Kunj Prajapati <kunjoza04@gmail.com>
2026-04-03 08:19:38 +0530

    Update collections.js

--- models/collections.js
+++ models/collections.js
@@ -1,6 +1,11 @@
+
 const { getDB } = require("../config/db");
 
-const teams      = () => getDB().collection("teams");
+const teams       = () => getDB().collection("teams");
 const leaderboard = () => getDB().collection("leaderboard");
+const teamChatCollection = (teamId) => {
+    const cleanId = String(teamId).replace(/[^a-zA-Z0-9_-]/g, "");
+    return getDB().collection(`chat_${cleanId}`);
+};
 
-module.exports = { teams, leaderboard };
+module.exports = { teams, leaderboard, teamChatCollection };
